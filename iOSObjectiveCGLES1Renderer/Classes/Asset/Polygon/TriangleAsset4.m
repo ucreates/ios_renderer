5b787d026f3d2c94c24918b12ea955c934b55765
ucreates <ucreates.inc@gmail.com>
2020-12-09 00:12:14 +0900

    add triangle4 texture component

--- iOSObjectiveCGLES1Renderer/Classes/Asset/Polygon/TriangleAsset4.m
+++ iOSObjectiveCGLES1Renderer/Classes/Asset/Polygon/TriangleAsset4.m
@@ -64,6 +64,64 @@
     [self->_vertex setIndicies:indicies indiciesCount:indiciesLength];
     return;
 }
+- (void)create:(NSString*)texturePath {
+    self->_texture = [[TextureAsset alloc] init];
+    [self->_texture load:texturePath];
+    GLfloat vratio = 1.0f - self->_texture.uvRatio.height;
+    GLfloat x = 0.5f * self->_width;
+    GLfloat y = 0.5f * self->_height;
+    GLfloat vertices[] = {
+        // left down
+        -x,
+        -y,
+        // right down
+        x,
+        -y,
+        // center top
+        0.0f,
+        y,
+    };
+    GLfloat colors[] = {
+        // left down
+        self->_color.r,
+        self->_color.g,
+        self->_color.b,
+        self->_color.a,
+        // right down
+        self->_color.r,
+        self->_color.g,
+        self->_color.b,
+        self->_color.a,
+        // center top
+        self->_color.r,
+        self->_color.g,
+        self->_color.b,
+        self->_color.a,
+    };
+    GLfloat uvs[] = {
+        //左下
+        0.0f,
+        1.0f,
+        //右下
+        self->_texture.uvRatio.width,
+        1.0f,
+        //中央上
+        0.5f * self->_texture.uvRatio.width,
+        vratio,
+    };
+    GLushort indicies[] = {0, 1, 2};
+    int verticesLength = sizeof(vertices) / sizeof(GLfloat);
+    int colorsLength = sizeof(colors) / sizeof(GLfloat);
+    int uvsLength = sizeof(uvs) / sizeof(GLfloat);
+    int indiciesLength = sizeof(indicies) / sizeof(GLushort);
+    int vertexCount = verticesLength / self.vertex.dimension;
+    [self->_vertex setVertexCount:vertexCount];
+    [self->_vertex setVerticies:vertices verticiesCount:verticesLength];
+    [self->_vertex setColors:colors vertexColorsCount:colorsLength];
+    [self->_vertex setUVs:uvs uvsCount:uvsLength];
+    [self->_vertex setIndicies:indicies indiciesCount:indiciesLength];
+    return;
+}
 - (GLenum)renderMode {
     return GL_TRIANGLE_STRIP;
 }
