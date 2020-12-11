a7179b0242fdcdff08c2a3b076b8882f258d6055
ucreates <ucreates.inc@gmail.com>
2020-12-12 00:02:24 +0900

    add rectangle4 texture component

--- iOSObjectiveCGLES1Renderer/Classes/Asset/Polygon/RectangleAsset4.m
+++ iOSObjectiveCGLES1Renderer/Classes/Asset/Polygon/RectangleAsset4.m
@@ -72,6 +72,75 @@
     [self->_vertex setIndicies:indicies indiciesCount:indiciesLength];
     return;
 }
+- (void)create:(NSString*)texturePath {
+    self->_texture = [[TextureAsset alloc] init];
+    [self->_texture load:texturePath];
+    GLfloat vratio = 1.0f - self->_texture.uvRatio.height;
+    float x = 0.5f * self->_width;
+    float y = 0.5f * self->_height;
+    GLfloat vertices[] = {
+        // left down
+        -x,
+        -y,
+        // right down
+        x,
+        -y,
+        // left top
+        -x,
+        y,
+        // right top
+        x,
+        y,
+    };
+    GLfloat colors[] = {
+        // left down
+        self->_color.r,
+        self->_color.g,
+        self->_color.b,
+        self->_color.a,
+        // right down
+        self->_color.r,
+        self->_color.g,
+        self->_color.b,
+        self->_color.a,
+        // left top
+        self->_color.r,
+        self->_color.g,
+        self->_color.b,
+        self->_color.a,
+        // right top
+        self->_color.r,
+        self->_color.g,
+        self->_color.b,
+        self->_color.a,
+    };
+    GLfloat uvs[] = {
+        //左下
+        0.0f,
+        1.0f,
+        //右下
+        1.0f * self->_texture.uvRatio.width,
+        1.0f,
+        //左上
+        0.0f,
+        0.0f + vratio,
+        //右上
+        1.0f * self->_texture.uvRatio.width,
+        0.0f + vratio,
+    };
+    GLushort indicies[] = {0, 1, 2, 3};
+    int verticesLength = sizeof(vertices) / sizeof(GLfloat);
+    int colorsLength = sizeof(colors) / sizeof(GLfloat);
+    int uvsLength = sizeof(uvs) / sizeof(GLfloat);
+    int indiciesLength = sizeof(indicies) / sizeof(GLushort);
+    int vertexCount = verticesLength / self.vertex.dimension;
+    [self->_vertex setVertexCount:vertexCount];
+    [self->_vertex setVerticies:vertices verticiesCount:verticesLength];
+    [self->_vertex setColors:colors vertexColorsCount:colorsLength];
+    [self->_vertex setUVs:uvs uvsCount:uvsLength];
+    [self->_vertex setIndicies:indicies indiciesCount:indiciesLength];
+    return;
+}
 - (GLenum)renderMode {
     return GL_TRIANGLE_STRIP;
 }
